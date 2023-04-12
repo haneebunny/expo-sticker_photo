14e5a23b62b1eb750e9ba7d8ef7499335eb806b1
HANI <re____@naver.com>
2023-04-12 16:49:17 +0900

    :lipstick: button 추가

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ImageViewer from './components/ImageViewer';
+import Button from './components/button';
 
 
 const PlaceholderImage = require('./assets/images/paduck.jpg')
@@ -11,7 +12,10 @@ export default function App() {
       <View style={styles.imageContainer}>
       <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
-      <Text style={{ color: '#fff' }}>Open up App.js to start working on your app!</Text>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="사진을 고르세요" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,14 +26,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
+  buttonContainer: {
     width: 320,
-    height: 440,
-    borderRadius: 18,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
