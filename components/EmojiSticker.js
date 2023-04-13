4d83397c091722af800ef2401b20d488e125e487
HANI <re____@naver.com>
2023-04-13 16:39:29 +0900

    :sparkles: 웹에서도 저장하자

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,5 +1,5 @@
 import { View, Image } from 'react-native'
-import { PanGestureHandler, TapGestureHandler } from "react-native-gesture-handler";
+import { PanGestureHandler, PinchGestureHandler, TapGestureHandler } from "react-native-gesture-handler";
 import Animated, {
     useAnimatedStyle,
     useSharedValue,
@@ -14,11 +14,18 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
     const translateX = useSharedValue(0);
     const translateY = useSharedValue(0);
     const scaleImage = useSharedValue(imageSize);
+    // const baseScale = Animated.Value(1);
+    // const pinchScale = Animated.Value(1);
+    // const scale = Animated.multiply(baseScale, pinchScale);
+    // const lastScale = 1;
+    // const onPinchGestureEvent = Animated.event([{ nativeEvent: { scale: pinchScale } },
+    // { useNativeDriver: USE_NATIVE_DRIVER }]);
+
 
     const onDoubleTap = useAnimatedGestureHandler({
         onActive: () => {
             if (scaleImage.value) {
-                scaleImage.value = scaleImage.value * 2;
+                scaleImage.value = scaleImage.value * 1.5;
             }
         },
     });
@@ -43,6 +50,14 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
     // onStart() : 제스처가 시작되거나 초기 위치에 있을 때
     // onActive() : 제스처가 활성화되어 있고 움직이는 경우 
 
+    // const onPinch = useAnimatedGestureHandler({
+    //     onActive: (event) => {
+    //         // if (scaleImage.value) {
+    //         //     scaleImage.value = event.translationX + event.translationY;
+    //         // }
+    //     }
+    // })
+
     const containerStyle = useAnimatedStyle(() => {
         return {
             transform: [
@@ -59,15 +74,18 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
 
     return (
         <PanGestureHandler onGestureEvent={onDrag}>
+            {/* <PinchGestureHandler onGestureEvent={onPinch}> */}
+
             <AnimatedView style={[containerStyle, { top: -350 }]}>
                 <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
                     <AnimatedImage
                         source={stickerSource}
-                        style={[imageStyle, { width: imageSize, height: imageSize }]}
-
+                        style={[imageStyle, { width: imageSize, height: imageSize, transform: [{ scale: 1 }] }]}
+                        resizeMode='contain'
                     />
                 </TapGestureHandler>
             </AnimatedView>
+            {/* </PinchGestureHandler> */}
         </PanGestureHandler>
     );
 }
